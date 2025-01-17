a9b9853beb22a5152527daa057a1f45caf14797f
mrkaan <mrkaan@qq.com>
2025-01-18 03:38:55 +0800

    feat: 解决拖拽问题

--- pages/render.js
+++ pages/render.js
@@ -1,45 +1,51 @@
-// 获取自定义标题栏元素
-const customTitleBar = document.getElementById('custom-title-bar');
+// import drag from 'electron-drag';
 
-let dragging = false;
-let initialClientX, initialClientY;
-let initialWindowX, initialWindowY;
+// // 获取自定义标题栏元素
+// const customTitleBar = document.getElementById('custom-title-bar');
 
-// 监听鼠标按下事件开始拖动
-customTitleBar.addEventListener('mousedown', async (e) => {
-  console.log('开始拖动窗口');
+// if (customTitleBar) {
+//     drag(customTitleBar);
+// }
+
+// let dragging = false;
+// let initialClientX, initialClientY;
+// let initialWindowX, initialWindowY;
+
+// // 监听鼠标按下事件开始拖动
+// customTitleBar.addEventListener('mousedown', async (e) => {
+//   console.log('开始拖动窗口');
   
-  // 发送消息给主进程请求窗口当前位置
-  let position = await electronAPI.get('get-window-position');
+//   // 发送消息给主进程请求窗口当前位置
+//   let position = await electronAPI.get('get-window-position');
   
-  if (position !== null && !dragging) {
-    dragging = true;
+//   if (position !== null && !dragging) {
+//     dragging = true;
 
-    // 记录初始的鼠标点击位置和窗口位置
-    initialClientX = e.clientX;
-    initialClientY = e.clientY;
-    initialWindowX = position.x;
-    initialWindowY = position.y;
-  }
-});
+//     // 记录初始的鼠标点击位置和窗口位置
+//     initialClientX = e.clientX;
+//     initialClientY = e.clientY;
+//     initialWindowX = position.x;
+//     initialWindowY = position.y;
+//   }
+// });
 
-document.addEventListener('mousemove', (e) => {
-  if (!dragging) return;
+// document.addEventListener('mousemove', (e) => {
+//   if (!dragging) return;
 
-  // 计算新的窗口位置
-  const deltaX = e.clientX - initialClientX;
-  const deltaY = e.clientY - initialClientY;
-  const newX = initialWindowX + deltaX;
-  const newY = initialWindowY + deltaY;
-  const data = { x: newX, y: newY };
+//   // 计算新的窗口位置
+//   const deltaX = e.clientX - initialClientX;
+//   const deltaY = e.clientY - initialClientY;
+//   const newX = initialWindowX + deltaX;
+//   const newY = initialWindowY + deltaY;
+//   const data = { x: newX, y: newY };
 
-  // 发送消息给主进程以更新窗口位置
-  electronAPI.send('update-window-position', data);
-});
+//   // 发送消息给主进程以更新窗口位置
+//   electronAPI.send('update-window-position', data);
+// });
 
-document.addEventListener('mouseup', () => {
-  dragging = false;
-});
+// document.addEventListener('mouseup', () => {
+//   dragging = false;
+// });
 
 
 
